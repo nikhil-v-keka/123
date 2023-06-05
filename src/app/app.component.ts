f4f4cd9cdb4244f17c0f98fc74226b0073bf2323
nikhil-v-keka <nikhil.v@keka.com>
2023-06-05 11:54:19 +0530

    initial commit 123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'sample-project';
+export class AppComponent implements OnInit {
+  title = 'test-app';
+  name: any = 'nikhil';
+  ngOnInit() {
+    if (this.name == 'nikhil') {
+      console.log('yes');
+    } else {
+    }
+
+    console.log('this is for testing : ');
+  }
+  users: User[] = [
+    {
+      name: 'John Doe',
+      job: 'Architect and Engineer',
+    },
+    {
+      name: 'Nikhil',
+      job: 'Movie Director',
+    },
+    {
+      name: 'Swetha',
+      job: 'Sports person',
+    },
+    {
+      name: 'Ram',
+      job: 'Quality Analyst',
+    },
+  ];
 }
