b363c8257386e4c3b38aa104fdc1afb8acbd6cda
nikhil-v-keka <nikhil.v@keka.com>
2023-06-06 10:35:56 +0530

    uploading files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,37 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { User } from './model/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'test-app';
-  name: any = 'nikhil';
-  ngOnInit() {
-    if (this.name == 'nikhil') {
-      console.log('yes');
-    } else {
-    }
+	title = 'My Application';
+	name: any = 'swetha';
+	ngOnInit() {
+		if (this.name == 'nikhil') {
+			console.log('yes');
+		} else {
+		}
 
-    console.log('this is for testing : ');
-  }
-  users: User[] = [
-    {
-      name: 'John Doe',
-      job: 'Architect and Engineer',
-    },
-    {
-      name: 'Nikhil',
-      job: 'Movie Director',
-    },
-    {
-      name: 'Swetha',
-      job: 'Sports person',
-    },
-    {
-      name: 'Ram',
-      job: 'Quality Analyst',
-    },
-  ];
+		console.log('this is for testing : ');
+	}
+	users: User[] = [
+		{
+			name: 'John Doe',
+			job: 'Architect and Engineer',
+		},
+		{
+			name: 'Nikhil',
+			job: 'Movie Director',
+		},
+		{
+			name: 'Swetha',
+			job: 'Sports person',
+		},
+		{
+			name: 'Ram',
+			job: 'Quality Analyst',
+		},
+	];
 }
